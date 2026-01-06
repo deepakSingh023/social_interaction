a730113383bbdda5963a834a2ad2dde61fb6b0f3
deepakSingh023 <dsinghrana71@gmail.com>
2026-01-06 12:21:55 +0530

    error handling

--- src/main/java/com/example/social_interaction/tasks/KeepAliveTask.java
+++ src/main/java/com/example/social_interaction/tasks/KeepAliveTask.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class KeepAliveTask {
 
-    private static final String PING_URL = "https://auth-social-1.onrender.com/api/health";
+    private static final String PING_URL = "https://social-interaction-by8w.onrender.com/api/health";
 
     private final RestTemplate restTemplate = new RestTemplate();
 
