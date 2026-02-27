df0473438b4dc085bfdb9bf941fa7ea5324cafc3
deepakSingh023 <dsinghrana71@gmail.com>
2026-02-27 21:02:03 +0530

    some abstracion in the service uri hiding part

--- src/main/java/com/example/social_interaction/tasks/KeepAliveTask.java
+++ src/main/java/com/example/social_interaction/tasks/KeepAliveTask.java
@@ -1,5 +1,6 @@
 package com.example.social_interaction.tasks;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,8 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class KeepAliveTask {
 
-    private static final String PING_URL = "https://social-interaction-x1t7.onrender.com/api/health";
+    @Value("${ping.uri}")
+    private static String PING_URL;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
